0d702f74cd79439fdf09da194e7cdfa05cecc26d
Haixing Hu <starfish.hu@gmail.com>
2026-05-02 16:21:59 +0800

    docs: normalize multi values setter license header

--- src/multi_values/multi_values_setter.rs
+++ src/multi_values/multi_values_setter.rs
@@ -1,12 +1,12 @@
 /*******************************************************************************
  *
- *    Copyright (c) 2025 - 2026.
- *    Haixing Hu, Qubit Co. Ltd.
+ *    Copyright (c) 2025 - 2026 Haixing Hu.
  *
- *    All rights reserved.
+ *    SPDX-License-Identifier: Apache-2.0
  *
-******************************************************************************/
-
+ *    Licensed under the Apache License, Version 2.0.
+ *
+ ******************************************************************************/
 //! Internal implementations for replacing a full `MultiValues` vector.
 
 use crate::value_error::ValueResult;
